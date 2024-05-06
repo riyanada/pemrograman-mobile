fc596e2a12bd785940e0683bdd4850993053c6f1
Riyan <riyanmaulana402@yahoo.co.id>
2024-05-07 00:35:18 +0700

    add week-4

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,105 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MainApp());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const appTitle = 'Form Styling Demo';
+    const String appTitle = 'Week 4';
+    FocusNode nodeOne = FocusNode();
+    FocusNode nodeTwo = FocusNode();
+
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       title: appTitle,
       home: Scaffold(
         appBar: AppBar(
           title: const Text(appTitle),
         ),
-        body: const MyCustomForm(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                focusNode: nodeOne,
+                decoration:
+                    const InputDecoration(prefixIcon: Icon(Icons.print)),
+              ),
+              TextField(
+                focusNode: nodeTwo,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  prefix: CircularProgressIndicator(),
+                ),
+              ),
+              TextButton(
+                child: Text("Button"),
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.green,
+                  side: BorderSide(color: Colors.deepOrange, width: 1),
+                  elevation: 20,
+                  minimumSize: Size(100, 50),
+                  shadowColor: Colors.red,
+                ),
+                onPressed: () {},
+              ),
+              TextButton.icon(
+                icon: Icon(Icons.save),
+                label: Text("TextButton"),
+                onPressed: () {},
+              ),
+              ElevatedButton(
+                child: Text("ElevatedButton"),
+                style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.purple,
+                    disabledForegroundColor: Colors.grey.withOpacity(0.38),
+                    disabledBackgroundColor: Colors.grey.withOpacity(0.12),
+                    side: BorderSide(color: Colors.black, width: 1),
+                    elevation: 20,
+                    minimumSize: Size(150, 50),
+                    shadowColor: Colors.teal,
+                    padding: EdgeInsets.only(
+                        left: 80, right: 80, top: 15, bottom: 15)),
+                onPressed: () {},
+              ),
+              OutlinedButton(
+                child: Text("OutlinedButton"),
+                style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.blueGrey,
+                    disabledForegroundColor:
+                        Colors.orangeAccent.withOpacity(0.38),
+                    side: BorderSide(color: Colors.deepOrange, width: 1),
+                    elevation: 20,
+                    minimumSize: Size(100, 50),
+                    shadowColor: Colors.deepOrange,
+                    padding:
+                        EdgeInsets.symmetric(vertical: 20, horizontal: 100)),
+                onPressed: () {},
+              ),
+              OutlinedButton.icon(
+                icon: Icon(Icons.save),
+                label: Text("Outline Icon"),
+                onPressed: () {},
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  FocusScope.of(context).requestFocus(nodeTwo);
+                },
+                child: const Text("Next Field"),
+              ),
+            ],
+          ),
+        ),
       ),
-    );
-  }
-}
-
-class MyCustomForm extends StatelessWidget {
-  const MyCustomForm({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Enter a search term',
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: TextFormField(
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Enter your username',
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
